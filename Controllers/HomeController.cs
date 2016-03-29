9f64103de2abd985ec5092713f6c4667175f949a
Shana Green <sgreen270@gmail.com>
2016-03-28 21:23:45 -0400

    Added Master Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace Mar28Git.Controllers
             Response.Write("Hello from Index Page.");
         }
 
+        public void MasterPage()
+        {
+            Response.Write("Hello from Master Page.");
+        }
+
     }
 }
